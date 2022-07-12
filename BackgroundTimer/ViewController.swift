d30b2c32f98a0b1ff6d5f216be6f761ec422e67c
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-07-12 12:48:17 +0300

    Add UI programmatically

--- BackgroundTimer/ViewController.swift
+++ BackgroundTimer/ViewController.swift
@@ -6,18 +6,70 @@
 //
 
 import UIKit
-import AloeStackView
+//import AloeStackView
 import TinyConstraints
 
-class ViewController: AloeStackViewController {
+class ViewController: UIViewController {
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.font = UIFont.systemFont(ofSize: 30.0)
+        return label
+    }()
+
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("START", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 30.0)
+        button.addTarget(self, action: #selector(tapStartButton), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("RESET", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 30.0)
+        button.addTarget(self, action: #selector(tapResetButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         view.backgroundColor = .red
+
+        addRows()
     }
-
-
 }
 
+
+private extension ViewController {
+    func addRows() {
+        do {
+            view.addSubview(label)
+            label.topToSuperview(offset: 100)
+            label.centerXToSuperview()
+        }
+
+        do {
+            view.addSubview(startButton)
+            startButton.bottom(to: label, offset: 50)
+            startButton.leadingToSuperview(offset: 50)
+        }
+
+        do {
+            view.addSubview(resetButton)
+            resetButton.bottom(to: label, offset: 50)
+            resetButton.trailingToSuperview(offset: 50)
+        }
+    }
+
+    @objc func tapStartButton() {
+        print(#function)
+    }
+
+    @objc func tapResetButton() {
+        print(#function)
+    }
+}
